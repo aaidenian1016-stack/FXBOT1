238bb81c4109d638f96463bd56c6853541603279
aaidenian1016-stack <aaidenian1016@gmail.com>
2026-05-11 19:59:35 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import feedparser
 import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-CHANNEL_ID = int(os.getenv("TELEGRAM_CHANNEL_ID"))
+TOKEN = os.getenv("8662151451:FpKPGA3E2BIJK")
+CHANNEL_ID = int(os.getenv("-1003916588968"))
 
 bot = telebot.TeleBot(TOKEN)
 
